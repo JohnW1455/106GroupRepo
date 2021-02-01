048f1df8f659fe0b1176ee7b1a56ac031daf95f9
elliot gong <ewcgong@hotmail.com>
2021-02-01 17:54:40 -0500

    Elliot - created actor class constructor and properties

--- HomeWork 1_Story Generator/Actor.cs
+++ HomeWork 1_Story Generator/Actor.cs
@@ -8,5 +8,32 @@ namespace HomeWork_1_Story_Generator
 {
     class Actor
     {
+        private string name;
+        private int age;
+        private string ethnicity;
+        public Actor(string name, int age, string ethnicity)
+        {
+            this.name = name;
+            this.age = age;
+            this.ethnicity = ethnicity;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value;}
+        }
+
+        public string Ethnicity
+        {
+            get { return ethnicity; }
+            set { ethnicity = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
     }
 }

e9273c8ccacb8326715ae0ae3b19d806ccee57dc
Lance Royer <lcr5451@rit.edu>
2021-02-01 18:37:05 -0700

    Entered skeleton of program.cs to be filled in with the precise code

--- HomeWork 1_Story Generator/Program.cs
+++ HomeWork 1_Story Generator/Program.cs
@@ -4,12 +4,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Start date 2/1/21
+
+//Lance Royer, Elliot Gong, John, Jack Doyle
+
+
+
 namespace HomeWork_1_Story_Generator
 {
+
+    //This is the main class to ask for a type of story and to output the story to the user
     class Program
     {
         static void Main(string[] args)
         {
-        }
+            //vars
+            string storyEnd;
+            string fullStory = "";
+            string repeatString;
+            bool validInput;
+            
+
+
+            Console.WriteLine("Welcome to the story generator!\n");
+
+
+            //Start loop 
+
+            //This repeats when the option is chosen to choice multiple stories
+            for (int i = 0; i < 1; i++)
+            {
+                //means the do - while will activate at least ounce
+                validInput = false;
+
+                do //while validInput == false
+                {
+
+                    //ask for type of story that is wanted
+                    Console.WriteLine("Please choose a type of ending to generate a story: \n'happy' 'tragic' 'romantic' \n'destructive' 'twist' 'any ending'\n\n");
+
+                    Console.WriteLine("Your choice >> ");
+                    string userInput = Console.ReadLine();
+
+                    //putt the input to lower to check for caps errors
+                    storyEnd = userInput.ToLower();
+
+                    switch (storyEnd)
+                    {
+                        case "happy":
+                            validInput = true;
+                            break;
+                        case "tragic":
+                            validInput = true;
+                            break;
+                        case "romantic":
+                            validInput = true;
+                            break;
+                        case "destructive":
+                            validInput = true;
+                            break;
+                        case "twist":
+                            validInput = true;
+                            break;
+                        case "any ending":
+                            validInput = true;
+                            break;
+                        default:
+                            Console.WriteLine("That is not one of the choices, please retry\n");
+                            
+                            break;
+                    }
+                } while (validInput == false);
+            
+
+                Console.WriteLine("\n");
+
+
+                ///find way to get the generated story
+
+
+
+
+                //Print out generated sotry
+                Console.WriteLine("Story Idea: ");
+                Console.WriteLine(fullStory);
+
+
+                //loop to make sure a valid answer is inputted
+                for (int k = 0; k < 1; k++)
+                {
+                    Console.WriteLine("Would you like another story? Choose 'yes' or 'no' >> ");
+                    repeatString = Console.ReadLine();
+
+                    //to lower to remove caps errors
+                    string stringLower = repeatString.ToLower();
+
+
+                    if (stringLower == "yes")
+                    {
+                        //restart major loop to restart the code
+                        i--;
+                    }
+                    else if (stringLower == "no")
+                    {
+                        Console.WriteLine("GoodBye!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid repsonse.");
+                        //restart this small loop because the answer is not yes or no
+                        k--;
+                    }
+                }//end of loop to make sure they answer yes or no
+
+            }//end of loop to run majority of code how ever manny time the user would like
+
+
+            //keep console open
+            Console.ReadKey();
+
+        }//end of main
     }
 }

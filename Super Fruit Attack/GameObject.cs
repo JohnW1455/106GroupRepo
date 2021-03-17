b100121b6260e726747cd2ca15ea4a9b563fbf9d
elliot gong <ewcgong@hotmail.com>
2021-03-16 21:27:52 -0400

    Elliot - added properties for the gameobject class

--- Super Fruit Attack/GameObject.cs
+++ Super Fruit Attack/GameObject.cs
@@ -17,5 +17,35 @@ namespace Super_Fruit_Attack
             this.image = image;
             box = new Rectangle(x, y, width, height);
         }
+
+        public Texture2D Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public int X
+        {
+            get { return box.X; }
+            set { box.X = value; }
+        }
+
+        public int Y
+        {
+            get { return box.Y; }
+            set { box.Y = value; }
+        }
+
+        public int Width
+        {
+            get { return box.Width; }
+            set { box.Width = value; }
+        }
+
+        public int Height
+        {
+            get { return box.Height; }
+            set { box.Height = value; }
+        }
     }
 }

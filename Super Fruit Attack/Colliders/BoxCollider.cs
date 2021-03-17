efae868193acaa67b3b2d21178e6bce68bd839ba
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-03-17 18:05:13 -0400

    Added colliders

---
+++ Super Fruit Attack/Colliders/BoxCollider.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+
+namespace Super_Fruit_Attack.Colliders
+{
+    //Author: Nathan Caron
+    //Date: 2/26/21
+    //Purpose: Represent a box collider
+    public class BoxCollider : Collider
+    {
+        public Rectangle Bounds => new Rectangle(Position.ToPoint(), Size);
+        
+        /// <summary>
+        /// Creates a new BoxCollider
+        /// </summary>
+        /// <param name="x">X location</param>
+        /// <param name="y">Y location</param>
+        /// <param name="width">Width</param>
+        /// <param name="height">Height</param>
+        public BoxCollider(int x, int y, int width, int height) :
+            base(x, y, width, height)
+        {
+        }
+
+        protected override bool CheckCollision(BoxCollider other)
+        {
+            return BoxBoxCollision(this, other);
+        }
+
+        protected override bool CheckCollision(CircleCollider other)
+        {
+            return BoxCircleCollision(this, other);
+        }
+
+        public override bool CheckCollision(Collider other)
+        {
+            return other.CheckCollision(this);
+        }
+    }
+}
---
+++ Super Fruit Attack/Colliders/CircleCollider.cs
@@ -0,0 +1,41 @@
+using Microsoft.Xna.Framework;
+
+namespace Super_Fruit_Attack.Colliders
+{
+    //Author: Nathan Caron
+    //Date: 2/26/21
+    //Purpose: Represents a circle collider
+    public class CircleCollider : Collider
+    {
+        public int Radius { get; }
+
+        public Point Center => Position.ToPoint() + new Point(Radius);
+
+        /// <summary>
+        /// Creates a new CircleCollider
+        /// </summary>
+        /// <param name="x">X location</param>
+        /// <param name="y">Y location</param>
+        /// <param name="radius">Radius of the circle</param>
+        public CircleCollider(int x, int y, int radius) :
+            base(x, y, radius * 2, radius * 2)
+        {
+            Radius = radius;
+        }
+
+        protected override bool CheckCollision(BoxCollider other)
+        {
+            return BoxCircleCollision(other, this);
+        }
+
+        protected override bool CheckCollision(CircleCollider other)
+        {
+            return CircleCircleCollision(this, other);
+        }
+
+        public override bool CheckCollision(Collider other)
+        {
+            return other.CheckCollision(this);
+        }
+    }
+}

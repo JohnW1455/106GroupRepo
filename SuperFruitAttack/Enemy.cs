7bf36a5c3727c28d5baf337b3bd2a1e458d3e312
John Wager <jhw5517@g.rit.edu>
2021-03-22 17:07:35 -0400

    Initial commit of Projectile class

--- SuperFruitAttack/Enemy.cs
+++ SuperFruitAttack/Enemy.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using SuperFruitAttack.Colliders;
 
 namespace SuperFruitAttack
 {

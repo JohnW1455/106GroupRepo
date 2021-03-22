667be9de542b438b49e04c89683205e8dc194c05
John Wager <jhw5517@g.rit.edu>
2021-03-22 16:51:28 -0400

    Initial commit of Enemy class

--- SuperFruitAttack/Enemy.cs
+++ SuperFruitAttack/Enemy.cs
@@ -14,10 +14,20 @@ namespace SuperFruitAttack
         private int moveSpeed;
         private Player player;
 
-        public Enemy(Texture2D pic) : base()
+        public Enemy(Texture2D pic, int pHealth, int speed, Player p, Colliders.Collider collide) : base(pic, collide)
         {
-
+            health = pHealth;
+            moveSpeed = speed;
+            player = p;
         }
+
+        // called when the enemy takes damage
+        public void TakeDamage(int dmg)
+        {
+            health -= dmg;
+        }
+
+        public abstract void Tick();
 
     }
 }

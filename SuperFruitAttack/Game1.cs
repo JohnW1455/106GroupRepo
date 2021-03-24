eed386050a4ed951d86a3b3d2df38656a25c1772
elliot gong <ewcgong@hotmail.com>
2021-03-24 11:22:45 -0400

    instantiated button object in game1 and set up finite state machine in update and draw

--- SuperFruitAttack/Game1.cs
+++ SuperFruitAttack/Game1.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace SuperFruitAttack
 {
@@ -12,6 +18,13 @@ namespace SuperFruitAttack
         private SpriteBatch _spriteBatch;
         private MouseState previousMouse;
         private GameStages status;
+        private GameObjectManager objectManager;
+        private Dictionary<string, Texture2D> images;
+        private Texture2D startButton;
+        private Texture2D instructionsButton;
+        private Button start;
+        
+        private Button instructions;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -23,6 +36,8 @@ namespace SuperFruitAttack
         {
             // TODO: Add your initialization logic here
             status = GameStages.menu;
+            objectManager = new GameObjectManager();
+            images = new Dictionary<string, Texture2D>();
             base.Initialize();
         }
 
@@ -30,8 +45,20 @@ namespace SuperFruitAttack
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             Level.LoadTextures(Content);
-
+            startButton = Content.Load<Texture2D>("start button");
+            instructionsButton = Content.Load<Texture2D>("Images/instructions");
+            start = new Button( startButton,
+                                _graphics.PreferredBackBufferWidth / 2 - startButton.Width / 2,
+                                _graphics.PreferredBackBufferHeight / 2 - 3 * startButton.Height,
+                                startButton.Width,
+                                startButton.Height);
+            instructions = new Button(instructionsButton,
+                                      _graphics.PreferredBackBufferWidth / 2 - instructionsButton.Width / 2,
+                                      _graphics.PreferredBackBufferHeight / 2 - 2 * instructionsButton.Height,
+                                      instructionsButton.Width,
+                                      instructionsButton.Height);
             // TODO: use this.Content to load your game content here
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,6 +70,11 @@ namespace SuperFruitAttack
             switch(status)
             {
                 case GameStages.menu:
+                    if(start.IsClicked(previousMouse) == true)
+                    {
+                        status = GameStages.gameplay;
+                    }
+                    
                     break;
                 case GameStages.instructions:
                     break;
@@ -64,8 +96,27 @@ namespace SuperFruitAttack
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            switch(status)
+            {
+                case GameStages.menu:
+                    break;
+                case GameStages.instructions:
+                    break;
+                case GameStages.gameplay:
+                    break;
+                case GameStages.transition:
+                    break;
+                case GameStages.winGame:
+                    break;
+                case GameStages.gameOver:
+                    break;
+            }
             base.Draw(gameTime);
+        }
+
+        public void NextLevel()
+        {
+
         }
     }
 }

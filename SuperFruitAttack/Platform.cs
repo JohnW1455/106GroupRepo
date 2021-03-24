cd2ac9eb353993b6ea3cfbf0e6c748772e3e79b3
John Wager <jhw5517@g.rit.edu>
2021-03-24 18:07:02 -0400

    Added Platfrom class and some platform collision handling

---
+++ SuperFruitAttack/Platform.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SuperFruitAttack.Colliders;
+
+namespace SuperFruitAttack
+{
+    class Platform : GameObject
+    {
+        public Platform(Texture2D pic, int pHealth, int speed, Colliders.Collider collide) : base(pic, collide)
+        {
+
+        }
+
+        public override bool CheckCollision(Collider objectCollide)
+        {
+            // First checks if the player is intersecting an obstacle
+            if (base.CheckCollision(objectCollide))
+            {
+                Rectangle entityRect = objectCollide.Bounds;
+                Rectangle platRect = collideObject.Bounds;
+
+                Rectangle resultant = Rectangle.Intersect(entityRect, platRect);
+
+				if (resultant.Width < resultant.Height)
+				{
+					// if player is intersecting from right
+					if ((platRect.X - entityRect.X) > 0)
+					{
+						// move player left
+						entityRect.X -= resultant.Width;
+					}
+					// if the player is intersecting from the left
+					else
+					{
+						// move player right
+						entityRect.X += resultant.Width;
+					}
+				}
+				else
+				{
+					// if the player is intersecting from above 
+					if ((platRect.Y - entityRect.Y) > 0)
+					{
+						// move player up
+						entityRect.Y -= resultant.Height;
+					}
+					// if the player is intersecting from below
+					else
+					{
+						// move player down
+						entityRect.Y += resultant.Height;
+					}
+				}
+
+				return true;
+			}
+			return false;
+		}
+    }
+}

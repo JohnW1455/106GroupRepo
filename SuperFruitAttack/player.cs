325bfe33911f7414312fe03eafa608bca7439c6d
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-04-01 17:56:28 -0400

    what is happening with git v2

--- SuperFruitAttack/player.cs
+++ SuperFruitAttack/player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using SuperFruitAttack.Colliders;
@@ -55,7 +55,7 @@ namespace SuperFruitAttack
         {
             health = playerHealth;
             moveSpeed = new Vector2(playerMS, 0);
-            gravity = new Vector2(0, .9f);
+            gravity = new Vector2(0, 0.9f);
             playerVelocity = new Vector2(0, 0);
             pState = PlayerState.faceRight;
             jumpVelocity = new Vector2(0, -15.0f);
@@ -287,7 +287,7 @@ namespace SuperFruitAttack
 		/// </summary>
 		public void ApplyGravity()
         {
-            // Adds the gravity to the player velocity
+            // Adds the acceleration to the player velocity
             playerVelocity += gravity;
             // Adds the velocity to the player position
             this.colliderObject.Position = this.colliderObject.Position + playerVelocity;

16f91fbc627a42a8061f1782961db57698333935
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-04-01 17:32:04 -0400

    Fixed some player stuff

--- SuperFruitAttack/player.cs
+++ SuperFruitAttack/player.cs
@@ -54,7 +54,7 @@ namespace SuperFruitAttack
         {
             health = playerHealth;
             moveSpeed = new Vector2(playerMS, 0);
-            gravity = new Vector2(0, 0.5f);
+            gravity = new Vector2(0, .9f);
             playerVelocity = new Vector2(0, 0);
             pState = PlayerState.faceRight;
             jumpVelocity = new Vector2(0, -15.0f);
@@ -285,9 +285,8 @@ namespace SuperFruitAttack
 		/// </summary>
 		public void ApplyGravity()
         {
-            // Adds the acceleration to the player velocity
-            Vector2 accel = new Vector2(0f, 0.3f);
-            playerVelocity += accel;
+            // Adds the gravity to the player velocity
+            playerVelocity += gravity;
             // Adds the velocity to the player position
             this.colliderObject.Position = this.colliderObject.Position + playerVelocity;
         }
